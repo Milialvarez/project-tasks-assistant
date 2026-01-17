15027c8368d7cc7dd0d4a7dbb8f551268c562749
milagros alvarez <milagrosalvarez2604@gmail.com>
2026-01-17 20:12:42 -0300

    fix(project post): fix error of naming attributes

--- app/api/routers/projects.py
+++ app/api/routers/projects.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+
 from app.core.database import get_db
 from app.application.projects.create_project import CreateProjectUseCase
 from app.infrastructure.db.repositories.project_repository import SqlAlchemyProjectRepository
@@ -7,24 +8,25 @@ from app.infrastructure.db.repositories.user_repository import SqlAlchemyUserRep
 
 router = APIRouter(prefix="/projects", tags=["Projects"])
 
+
 @router.post("/")
 def create_project(
     name: str,
     description: str | None = None,
     db: Session = Depends(get_db),
 ):
-    project_repo = SqlAlchemyProjectRepository(db)
-    user_repo = SqlAlchemyUserRepository(db)
+    project_repository = SqlAlchemyProjectRepository(db)
+    user_repository = SqlAlchemyUserRepository(db)
 
     use_case = CreateProjectUseCase(
-        project_repo=project_repo,
-        user_repo=user_repo,
+        project_repository=project_repository,
+        user_repository=user_repository,
     )
 
     project = use_case.execute(
         name=name,
         description=description,
-        created_by=1,  # hardcodeado por hasta que existan usuarios y sesiones
+        created_by=1,  # hardcodeado por ahora
     )
 
     return project
--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 
+from app.api.routers import projects
+
 app = FastAPI(
     title="Project & Tasks Assistant API",
     description="Backend for project and task management",
     version="0.1.0",
 )
 
+app.include_router(projects.router)
+
 @app.get("/")
 def health_check():
     return {"status": "ok"}
+
